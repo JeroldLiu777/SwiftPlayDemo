508b8b82e17f39051eb39c24667ddcefaa069623
Jerold <jerold717@outlook.com>
2020-05-31 10:23:35 +0800

    Initial Commit

---
+++ SwiftPlay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftPlay
+//
+//  Created by jeroldliu on 2020/5/31.
+//  Copyright © 2020 jeroldliu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
